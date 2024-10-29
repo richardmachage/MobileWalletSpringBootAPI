c46a251510c305bf06573bb157b95aff53358cb9
Richard Machage <rchacha261@gmail.com>
2024-10-29 16:23:10 +0300

    Fix : (Transaction Repository) -> last-100-transactions, mini-statement

--- src/main/java/com/comulynx/wallet/rest/api/Application.java
+++ src/main/java/com/comulynx/wallet/rest/api/Application.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 	
-	//FIXME : Add Spring boot Dev Tools in the pom.xml file -> done
-	//FIXME : Add h2 database in the pom.xml file -> done
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
--- src/main/java/com/comulynx/wallet/rest/api/repository/TransactionRepository.java
+++ src/main/java/com/comulynx/wallet/rest/api/repository/TransactionRepository.java
@@ -3,8 +3,10 @@ package com.comulynx.wallet.rest.api.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.hibernate.annotations.Parameter;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.comulynx.wallet.rest.api.model.Transaction;
@@ -19,9 +21,12 @@ public interface TransactionRepository extends JpaRepository<Transaction, Long> 
 	Optional<List<Transaction>> findTransactionsByCustomerIdOrTransactionId(String transactionId, String customerId);
 
 	// TODO : Change below Query to return the last 5 transactions
-	// TODO : Change below Query to use Named parameters instead of indexed parameters
-	// TODO : Change below function to return Optional<List<Transaction>>
-	@Query("SELECT t FROM Transaction t WHERE t.customerId =?1 AND  t.accountNo =?2")
-	List<Transaction> getMiniStatementUsingCustomerIdAndAccountNo(String customer_id, String account_no);
+	// TODO : Change below Query to use Named parameters instead of indexed parameters -> done
+	// TODO : Change below function to return Optional<List<Transaction>> -> done
+	@Query("SELECT t FROM Transaction t WHERE t.customerId = :customer_id AND  t.accountNo = :account_no")
+	Optional<List<Transaction>> getMiniStatementUsingCustomerIdAndAccountNo(
+			@Param("customer_id") String customer_id,
+			@Param("account_no") String account_no
+	);
 
 }
